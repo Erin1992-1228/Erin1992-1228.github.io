92aac0a190b7db3b47385f367daa40ab5c7c10ff
Erin1992-1228 <siakdillag369@gmail.com>
2026-07-22 01:12:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // Erin Website Script
+
+function changeLang(lang){
+
+    if(lang === "zh"){
+        document.querySelectorAll(".zh").forEach(function(el){
+            el.style.display = "block";
+        });
+
+        document.querySelectorAll(".en").forEach(function(el){
+            el.style.display = "none";
+        });
+
+    }else{
+
+        document.querySelectorAll(".zh").forEach(function(el){
+            el.style.display = "none";
+        });
+
+        document.querySelectorAll(".en").forEach(function(el){
+            el.style.display = "block";
+        });
+    }
+
+}
